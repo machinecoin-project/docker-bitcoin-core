ecdafc6e6ef0018edfb04637ae4786ae02212e11
Nigho <nico.lucciola@googlemail.com>
2018-05-10 18:31:36 +0200

    Create directory first, then config file

--- 0.16/docker-entrypoint.sh
+++ 0.16/docker-entrypoint.sh
@@ -1,22 +1,6 @@
 #!/bin/sh
 set -e
 
-cat >${MACHINECOIN_CONFIG} <<EOF
-server=1
-
-rpcuser=${MACHINECOIN_RPCUSER:-machinecoin}
-rpcpassword=${MACHINECOIN_RPCPASSWORD:-changemeplzasap}
-rpcallowip=${MACHINECOIN_RPCALLOWIP:-127.0.0.1}
-
-printtoconsole=${MACHINECOIN_PRINTTOCONSOLE:-1}
-
-masternode=${MACHINECOIN_MASTERNODE}
-masternodeprivkey=${MACHINECOIN_MASTERNODE_KEY}
-externalip=${MACHINECOIN_MASTERNODE_IP}:40333
-
-txindex=1
-EOF
-
 
 if [ $(echo "$1" | cut -c1) = "-" ]; then
   echo "$0: assuming arguments for machinecoind"
@@ -33,6 +17,22 @@ if [ $(echo "$1" | cut -c1) = "-" ] || [ "$1" = "machinecoind" ]; then
 
   set -- "$@" -datadir="$MACHINECOIN_DATA" -conf="$MACHINECOIN_CONFIG"
 fi
+
+cat >${MACHINECOIN_CONFIG} <<EOF
+server=1
+
+rpcuser=${MACHINECOIN_RPCUSER:-machinecoin}
+rpcpassword=${MACHINECOIN_RPCPASSWORD:-changemeplzasap}
+rpcallowip=${MACHINECOIN_RPCALLOWIP:-127.0.0.1}
+
+printtoconsole=${MACHINECOIN_PRINTTOCONSOLE:-1}
+
+masternode=${MACHINECOIN_MASTERNODE}
+masternodeprivkey=${MACHINECOIN_MASTERNODE_KEY}
+externalip=${MACHINECOIN_MASTERNODE_IP}:40333
+
+txindex=1
+EOF
 
 if [ "$1" = "machinecoind" ] || [ "$1" = "machinecoin-cli" ] || [ "$1" = "machinecoin-tx" ]; then
   echo
