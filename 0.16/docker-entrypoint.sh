916244ae8adefeb1327239d81bf8f234ee0ebc26
Nigho <nico.lucciola@googlemail.com>
2018-05-11 09:40:11 +0200

    Reworked config file creation

--- 0.16/docker-entrypoint.sh
+++ 0.16/docker-entrypoint.sh
@@ -12,26 +12,26 @@ if [ $(echo "$1" | cut -c1) = "-" ] || [ "$1" = "machinecoind" ]; then
   mkdir -p "$MACHINECOIN_DATA"
   chmod 700 "$MACHINECOIN_DATA"
   chown -R machinecoin "$MACHINECOIN_DATA"
-  
-  cat >${MACHINECOIN_CONFIG} <<EOF
-server=1
 
-rpcuser=${MACHINECOIN_RPCUSER:-machinecoin}
-rpcpassword=${MACHINECOIN_RPCPASSWORD:-changemeplzasap}
-rpcallowip=${MACHINECOIN_RPCALLOWIP:-127.0.0.1}
+  cat <<-EOF > "$MACHINECOIN_DATA/machinecoin.conf"
+  server=1
 
-printtoconsole=${MACHINECOIN_PRINTTOCONSOLE:-1}
+  rpcuser=${MACHINECOIN_RPCUSER:-machinecoin}
+  rpcpassword=${MACHINECOIN_RPCPASSWORD:-changemeplzasap}
+  rpcallowip=${MACHINECOIN_RPCALLOWIP:-127.0.0.1}
 
-masternode=${MACHINECOIN_MASTERNODE:-0}
-masternodeprivkey=${MACHINECOIN_MASTERNODE_KEY:-0}
-externalip=${MACHINECOIN_MASTERNODE_IP:-0}:40333
+  printtoconsole=${MACHINECOIN_PRINTTOCONSOLE:-1}
 
-txindex=1
-EOF
+  masternode=${MACHINECOIN_MASTERNODE:-0}
+  masternodeprivkey=${MACHINECOIN_MASTERNODE_KEY:-0}
+  externalip=${MACHINECOIN_MASTERNODE_IP:-0}:40333
+
+  txindex=1
+		EOF
 
   echo "$0: setting data directory to $MACHINECOIN_DATA"
 
-  set -- "$@" -datadir="$MACHINECOIN_DATA" -conf="$MACHINECOIN_CONFIG"
+  set -- "$@" -datadir="$MACHINECOIN_DATA"
 fi
 
 if [ "$1" = "machinecoind" ] || [ "$1" = "machinecoin-cli" ] || [ "$1" = "machinecoin-tx" ]; then
